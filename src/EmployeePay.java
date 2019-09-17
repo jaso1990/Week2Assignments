9777e68081296f94ffdeac999217ebe966e864a1
jaso1990 <jaso1990@ISS2TZQKH2.scdsb.pri>
2019-09-17 11:08:56 -0400

    Initial commit

---
+++ src/EmployeePay.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class EmployeePay {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        int hours; 
+        final double OVER = 22.5;
+        final double REG = 15;
+    }
+    
+}
